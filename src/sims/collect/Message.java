a4fef237a039da023047d686a2cbe4d3e0307cb5
Hu Yingcong <me@huyc.info>
2020-10-29 11:10:08 +0800

    move plumtree message

--- src/sims/collect/Message.java
+++ src/sims/collect/Message.java
@@ -48,3 +48,21 @@ public class Message implements Cloneable {
         
     }
 }
+
+class PlumtreeMessage extends Message {
+    Boolean isGossip = false;
+    Boolean isGraft  = false;
+    Boolean isPrune  = false;
+    Boolean isIHave  = false;
+
+    public String toString() {
+        return "@" + Integer.toHexString(System.identityHashCode(this))+"{" +
+        "id:" + id + "," + 
+        "from:" + fromNodeIndex + "," + 
+        (isGossip ? "gossip,": "") +
+        (isGraft  ? "graft," : "") +
+        (isPrune  ? "prune," : "") +
+        (isIHave  ? "ihave," : "") +
+        "}";
+    }
+}
